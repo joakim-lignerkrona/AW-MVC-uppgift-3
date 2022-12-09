b4eb84c228b44acc9050fcc7533235f7424fcb3d
Isak Asproth <isakasproth@gmail.com>
2022-12-09 15:45:13 +0100

    New

--- AW-MVC-uppgift-3/Program.cs
+++ AW-MVC-uppgift-3/Program.cs
@@ -2,7 +2,6 @@ using AW_MVC_uppgift_3.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-builder.Services.AddTransient<DataService>(); // Registrera för DI - ny instans varje gång
 builder.Services.AddSingleton<DataService>(); // Registrera för DI - samma instans varje gång
 var app = builder.Build();
 
