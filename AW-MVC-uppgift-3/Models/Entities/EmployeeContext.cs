5edfe27101fe5b6457e93aee250df58f8627f9d2
Martin Billberg <martinmidas@gmail.com>
2022-12-14 15:21:52 +0100

    testar lägga upp

--- AW-MVC-uppgift-3/Models/Entities/EmployeeContext.cs
+++ AW-MVC-uppgift-3/Models/Entities/EmployeeContext.cs
@@ -11,8 +11,18 @@ public partial class EmployeeContext : DbContext
     {
     }
 
+    public virtual DbSet<Employee> Employees { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Employee__3214EC070889E72B");
+
+            entity.Property(e => e.Email).HasMaxLength(50);
+            entity.Property(e => e.Name).HasMaxLength(30);
+        });
+
         OnModelCreatingPartial(modelBuilder);
     }
 
